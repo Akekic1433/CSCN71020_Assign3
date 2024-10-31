0e8d26ea7dac6ae2bbb5f7c16680400b40a8b025
Akekic1433 <Akekic1433@conestogac.on.ca>
2024-10-30 22:08:35 -0400

    Added pass for Player1 and Player2

--- MainProject/main.c
+++ MainProject/main.c
@@ -6,6 +6,30 @@ char* playRPS(char* player1, char* player2) {
 		return "Draw";
 	}
 
+	if (!strcmp(player1, "rock")) {
+		if (!strcmp(player2, "paper")) {
+			return "Player2";
+		}
+		else if (!strcmp(player2, "scissors")) {
+			return "Player1";
+		}
+	}
+	if (!strcmp(player1, "paper")) {
+		if (!strcmp(player2, "scissors")) {
+			return "Player2";
+		}
+		else if (!strcmp(player2, "rock")) {
+			return "Player1";
+		}
+	}
+	if (!strcmp(player1, "scissors")) {
+		if (!strcmp(player2, "rock")) {
+			return "Player2";
+		}
+		else if (!strcmp(player2, "paper")) {
+			return "Player1";
+		}
+	}
 	return "";
 }
 
